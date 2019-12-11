d88911d19c8bebe14c80e5e6b67290d547e1ad4f
Ubuntu <ubuntu@ip-172-31-95-131.ec2.internal>
2019-12-11 11:00:55 +0000

    print statement remove

--- pythia/modules/attention.py
+++ pythia/modules/attention.py
@@ -134,7 +134,7 @@ class TopDownAttention(nn.Module):
         return attention
 
     def forward(self, image_feat, question_embedding, image_locs=None):
-        print("[ topdown attention ] runs")
+        # print("[ topdown attention ] runs")
         # N x K x joint_dim
         joint_feature = self.combination_layer(image_feat, question_embedding)
         # N x K x n_att
